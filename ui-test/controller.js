9524d51afb3d53ff091d6af6a2c8a851aad2167f
Daniel J. Montanez <daniel.j.w.montanez.ctr@navy.mil>
2016-09-30 11:10:37 -0700

    Styling just about done

---
+++ ui-test/controller.js
@@ -0,0 +1,140 @@
+angular.module('app')
+  .controller('ctrl', ['$scope', function($scope) {
+  
+    $scope.messages = [
+      {
+        "alertid" : "24d6b8b5-a658-4561-8895-126e505a0ea2",
+        "acknowledged" : false,
+        "alertsource" : "S2A Test Harness",
+        "classification" : "Classification of Rule",
+        "created" : "2015-12-08T01:27:38.846+0000",
+        "alertDtg" : "2015-12-08T01:27:38.846+0000",
+        "createdDate" : "2015-12-08T01:27:39.004+0000",
+        "details" : "[{\"ShipName|key\":\"id\",\"value\":\"USS Neversail\"},{\"ShipName|key\":\"store\",\"value\":\"store information\"},{\"ShipName|key\":\"sma\",\"value\":\"0.0\"},{\"MMSI|key\":\"id\",\"value\":\"2222\"},{\"MMSI|key\":\"store\",\"value\":\"store information\"}]",
+        "recipient" : "TRAN.HOANH.T.1231122563",
+        "title" : "Title of Rule",
+        "geoAlert" : {
+          "featureid" : "4CF5492E-4FE5-475B-B926-072867D284F3",
+          "featurename" : "USS Neversail",
+          "lat" : -17.5,
+          "lon" : -170.0,
+          "createdDate" : "2015-12-08T01:27:39.004+0000"
+        },
+        "applicationSource" : "Intellex Test Harness",
+        "alertPk" : "80310591-517f-1af8-8151-7f337afc0056"
+      },
+      {
+        "alertid" : "24d6b8b5-a658-4561-8895-126e505a0ea2",
+        "acknowledged" : false,
+        "alertsource" : "S2A Test Harness",
+        "classification" : "Classification of Rule",
+        "created" : "2015-12-08T01:27:38.846+0000",
+        "alertDtg" : "2015-12-08T01:27:38.846+0000",
+        "createdDate" : "2015-12-08T01:27:39.004+0000",
+        "details" : "[{\"ShipName|key\":\"id\",\"value\":\"USS Neversail\"},{\"ShipName|key\":\"store\",\"value\":\"store information\"},{\"ShipName|key\":\"sma\",\"value\":\"0.0\"},{\"MMSI|key\":\"id\",\"value\":\"2222\"},{\"MMSI|key\":\"store\",\"value\":\"store information\"}]",
+        "recipient" : "TRAN.HOANH.T.1231122563",
+        "title" : "Title of Rule",
+        "geoAlert" : {
+          "featureid" : "4CF5492E-4FE5-475B-B926-072867D284F3",
+          "featurename" : "USS Neversail",
+          "lat" : -17.5,
+          "lon" : -170.0,
+          "createdDate" : "2015-12-08T01:27:39.004+0000"
+        },
+        "applicationSource" : "Intellex Test Harness",
+        "alertPk" : "80310591-517f-1af8-8151-7f337afc0056"
+      },
+      {
+        "alertid" : "24d6b8b5-a658-4561-8895-126e505a0ea2",
+        "acknowledged" : false,
+        "alertsource" : "S2A Test Harness",
+        "classification" : "Classification of Rule",
+        "created" : "2015-12-08T01:27:38.846+0000",
+        "alertDtg" : "2015-12-08T01:27:38.846+0000",
+        "createdDate" : "2015-12-08T01:27:39.004+0000",
+        "details" : "[{\"ShipName|key\":\"id\",\"value\":\"USS Neversail\"},{\"ShipName|key\":\"store\",\"value\":\"store information\"},{\"ShipName|key\":\"sma\",\"value\":\"0.0\"},{\"MMSI|key\":\"id\",\"value\":\"2222\"},{\"MMSI|key\":\"store\",\"value\":\"store information\"}]",
+        "recipient" : "TRAN.HOANH.T.1231122563",
+        "title" : "Title of Rule",
+        "geoAlert" : {
+          "featureid" : "4CF5492E-4FE5-475B-B926-072867D284F3",
+          "featurename" : "USS Neversail",
+          "lat" : -17.5,
+          "lon" : -170.0,
+          "createdDate" : "2015-12-08T01:27:39.004+0000"
+        },
+        "applicationSource" : "Intellex Test Harness",
+        "alertPk" : "80310591-517f-1af8-8151-7f337afc0056"
+      },
+      {
+        "alertid" : "24d6b8b5-a658-4561-8895-126e505a0ea2",
+        "acknowledged" : false,
+        "alertsource" : "S2A Test Harness",
+        "classification" : "Classification of Rule",
+        "created" : "2015-12-08T01:27:38.846+0000",
+        "alertDtg" : "2015-12-08T01:27:38.846+0000",
+        "createdDate" : "2015-12-08T01:27:39.004+0000",
+        "details" : "[{\"ShipName|key\":\"id\",\"value\":\"USS Neversail\"},{\"ShipName|key\":\"store\",\"value\":\"store information\"},{\"ShipName|key\":\"sma\",\"value\":\"0.0\"},{\"MMSI|key\":\"id\",\"value\":\"2222\"},{\"MMSI|key\":\"store\",\"value\":\"store information\"}]",
+        "recipient" : "TRAN.HOANH.T.1231122563",
+        "title" : "Title of Rule",
+        "geoAlert" : {
+          "featureid" : "4CF5492E-4FE5-475B-B926-072867D284F3",
+          "featurename" : "USS Neversail",
+          "lat" : -17.5,
+          "lon" : -170.0,
+          "createdDate" : "2015-12-08T01:27:39.004+0000"
+        },
+        "applicationSource" : "Intellex Test Harness",
+        "alertPk" : "80310591-517f-1af8-8151-7f337afc0056"
+      },
+      {
+        "alertid" : "24d6b8b5-a658-4561-8895-126e505a0ea2",
+        "acknowledged" : false,
+        "alertsource" : "S2A Test Harness",
+        "classification" : "Classification of Rule",
+        "created" : "2015-12-08T01:27:38.846+0000",
+        "alertDtg" : "2015-12-08T01:27:38.846+0000",
+        "createdDate" : "2015-12-08T01:27:39.004+0000",
+        "details" : "[{\"ShipName|key\":\"id\",\"value\":\"USS Neversail\"},{\"ShipName|key\":\"store\",\"value\":\"store information\"},{\"ShipName|key\":\"sma\",\"value\":\"0.0\"},{\"MMSI|key\":\"id\",\"value\":\"2222\"},{\"MMSI|key\":\"store\",\"value\":\"store information\"}]",
+        "recipient" : "TRAN.HOANH.T.1231122563",
+        "title" : "Title of Rule",
+        "geoAlert" : {
+          "featureid" : "4CF5492E-4FE5-475B-B926-072867D284F3",
+          "featurename" : "USS Neversail",
+          "lat" : -17.5,
+          "lon" : -170.0,
+          "createdDate" : "2015-12-08T01:27:39.004+0000"
+        },
+        "applicationSource" : "Intellex Test Harness",
+        "alertPk" : "80310591-517f-1af8-8151-7f337afc0056"
+      },
+      {
+        "alertid" : "24d6b8b5-a658-4561-8895-126e505a0ea2",
+        "acknowledged" : false,
+        "alertsource" : "S2A Test Harness",
+        "classification" : "Classification of Rule",
+        "created" : "2015-12-08T01:27:38.846+0000",
+        "alertDtg" : "2015-12-08T01:27:38.846+0000",
+        "createdDate" : "2015-12-08T01:27:39.004+0000",
+        "details" : "[{\"ShipName|key\":\"id\",\"value\":\"USS Neversail\"},{\"ShipName|key\":\"store\",\"value\":\"store information\"},{\"ShipName|key\":\"sma\",\"value\":\"0.0\"},{\"MMSI|key\":\"id\",\"value\":\"2222\"},{\"MMSI|key\":\"store\",\"value\":\"store information\"}]",
+        "recipient" : "TRAN.HOANH.T.1231122563",
+        "title" : "Title of Rule",
+        "geoAlert" : {
+          "featureid" : "4CF5492E-4FE5-475B-B926-072867D284F3",
+          "featurename" : "USS Neversail",
+          "lat" : -17.5,
+          "lon" : -170.0,
+          "createdDate" : "2015-12-08T01:27:39.004+0000"
+        },
+        "applicationSource" : "Intellex Test Harness",
+        "alertPk" : "80310591-517f-1af8-8151-7f337afc0056"
+      }
+    ];
+    
+    $scope.messages.forEach(function( message ) {
+      message.details = message.details.replace(/\\/g,"\\\\");
+    });
+    $scope.messages.forEach(function( message ) {
+      message.details = JSON.parse(message.details);
+    });
+  
+  }]);
